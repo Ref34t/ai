828232f2c0a4b67e1d6c1f735e59da98a003c6f0
Mohamed Khaled <mohamed.khaled@9hdigital.com>
2025-10-28 17:54:44 +0300

    Adjust: Example feature test triggers rest API init

--- tests/Integration/Includes/Features/Example_Feature/Example_FeatureTest.php
+++ tests/Integration/Includes/Features/Example_Feature/Example_FeatureTest.php
@@ -30,6 +30,7 @@ class Example_FeatureTest extends WP_UnitTestCase {
 		$loader   = new Feature_Loader( $registry );
 		$loader->register_default_features();
 		$loader->initialize_features();
+		do_action( 'rest_api_init' );
 
 		$feature = $registry->get_feature( 'example-feature' );
 		$this->assertInstanceOf( Example_Feature::class, $feature, 'Example feature should be registered in the registry.' );
